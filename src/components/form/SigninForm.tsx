498efcf588e5b1558a7b0c04008745ddc83c57bd
bb-grizzle <firbigi1993@gmail.com>
2023-02-28 00:25:24 +0900

    ADD | change page when email sent

--- src/components/form/SigninForm.tsx
+++ src/components/form/SigninForm.tsx
@@ -1,22 +1,49 @@
 import useInputDefault from "@/hook/useInputDefault";
 import fbSendSigninLink from "@/lib/firebase/auth/fbSendSigninLink";
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import InputText from "../input/InputText";
 import Button, { BtnTypeEnum } from "../shared/Button";
 
 const SigninForm = () => {
 	const emailHook = useInputDefault({ inputOption: { name: "email", placeholder: "type your email" } });
+	const [isSend, setIsSend] = useState(false);
+
+	useEffect(() => {
+		return () => {
+			setIsSend(false);
+		};
+	}, []);
+
+	const back = () => {
+		setIsSend(false);
+	};
+
+	const sendEmail = async () => {
+		if (!emailHook.value) return;
+		await fbSendSigninLink({ email: `${emailHook.value}` });
+		setIsSend(true);
+	};
 
 	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		if (!emailHook.value) return;
-		await fbSendSigninLink({ email: `${emailHook.value}` });
+		sendEmail();
 	};
 
 	return (
 		<form onSubmit={onSubmit}>
-			<InputText {...emailHook} />
-			<Button text="send email" iconOption={{ name: "navigate-outline" }} reverse={true} btnType={BtnTypeEnum.LINE} />
+			{!isSend ? (
+				<>
+					<InputText {...emailHook} />
+					<Button text="send email" iconOption={{ name: "navigate-outline" }} reverse={true} btnType={BtnTypeEnum.LINE} onClick={sendEmail} />
+				</>
+			) : (
+				<>
+					<p>check your mail box</p>
+
+					<Button text="type another email" iconOption={{ name: "arrow-back" }} reverse={true} btnType={BtnTypeEnum.LINE} onClick={back} />
+					<Button text="resend" iconOption={{ name: "refresh" }} reverse={true} btnType={BtnTypeEnum.LINE} onClick={sendEmail} />
+				</>
+			)}
 		</form>
 	);
 };
