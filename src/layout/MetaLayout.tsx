0cfc847f6e14fedea190f6fcdcf07bded1465f9b
bb-grizzle <firbigi1993@gmail.com>
2023-03-03 18:04:51 +0900

    -

--- src/layout/MetaLayout.tsx
+++ src/layout/MetaLayout.tsx
@@ -10,13 +10,16 @@ const MetaLayout: React.FC<MetaLayoutProps> = ({ children }) => {
 		<>
 			<Head>
 				<title>{DATA_META.title}</title>
-				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-				<link rel="manifest" href="/favicon/site.webmanifest" />
-				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-				<meta name="msapplication-TileColor" content="#2b5797" />
-				<meta name="theme-color" content="#ffffff" />
+
+				<meta
+					name="viewport"
+					content="height=device-height, 
+				width=device-width, 
+				initial-scale=1.0, 
+				minimum-scale=1.0, 
+				maximum-scale=1.0, 
+				user-scalable=no"
+				></meta>
 
 				<meta property="og:type" content="website" />
 				<meta property="og:url" content={DATA_META.url} />
@@ -26,6 +29,15 @@ const MetaLayout: React.FC<MetaLayoutProps> = ({ children }) => {
 				<meta property="og:site_name" content={DATA_META.site_name} />
 				<meta property="og:image:width" content="1200" />
 				<meta property="og:image:height" content="630" />
+
+				{/* favicon */}
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+				<meta name="msapplication-TileColor" content="#2b5797" />
+				<meta name="theme-color" content="#ffffff" />
 			</Head>
 			{children}
 		</>
